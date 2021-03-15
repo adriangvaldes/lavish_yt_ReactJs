73ada3c5294aaa3d7fd4978c9aa6ca37f9b47f3a
adriangvaldes <adriangvaldes@gmail.com>
2021-03-15 00:35:58 -0300

    Button.jss and .css creation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,25 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa';
+import '../styles/Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  window.addEventListener('resize', showButton);
   
   return (
     <div className="navbar">
@@ -34,7 +48,23 @@ function Navbar() {
               Products
             </Link>
           </li>
-          
+          <li className="nav-btn">
+            {button ? (
+              <Link to='/sign-up' className="btn-link">
+                <Button buttonStyle='btn--outline'>
+                  SIGN UP
+                </Button>
+              </Link>
+            ) : (
+              <Link to='/sign-up' className="btn-link">
+                <Button 
+                  buttonStyle='btn--outline' 
+                  buttonSize='btn--mobile'>
+                  SIGN UP
+                </Button>
+              </Link>
+            )}
+          </li>
         </ul>
       </div>
     </div>

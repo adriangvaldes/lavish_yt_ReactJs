a2b892dd3f29d782a932e2c4a98f49e9a4b2c96c
adriangvaldes <adriangvaldes@gmail.com>
2021-03-14 23:53:27 -0300

    navbar update v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hellow World</h1>
-    </div>
+    <Router>
+     <Navbar />
+     <Switch>
+       <Route path='/' />
+     </Switch>
+    </Router>
   );
 }
 
